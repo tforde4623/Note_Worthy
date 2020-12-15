fafc89cb4f1fd36219b0091f589fae533acd64ea
tt4623 <sltebr33zy@gmail.com>
2020-12-14 20:55:43 -0600

    fixed promise issues

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const data = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = app => {
   // GET api routes
   app.get('/api/notes', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(data));
+    fs.readFile('./db/db.json', (err, content) => {
+      res.json(JSON.parse(content));
+    })
   });
 
   // POST api routes
   app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, content) => {
+      res.json(JSON.parse(content));
       const json = JSON.parse(content);
+      req.body.id = uuidv4();
       json.push(req.body);
       // // write file with new data
-      fs.writeFile('./db/db.json', JSON.stringify(json), () => console.log('Wrote to file!'));
-      res.send(json); // return new data
+      fs.writeFile('./db/db.json', JSON.stringify(json), (err) => { if (err) console.log(err);});
     });
   });
 
@@ -25,8 +29,9 @@ module.exports = app => {
       const newData = JSON.parse(content)
         .filter(obj => obj.id !== req.params.id);
       // write new data back to file
+      res.json(JSON.parse(content));
       console.log(newData);
-      fs.writeFile('./db/db.json', JSON.stringify(newData), () => console.log(`Note ${req.params.id} deleted.`))
+      fs.writeFile('./db/db.json', JSON.stringify(newData), () => (err) => { if (err) console.log(err);})
     });
   });
 };
