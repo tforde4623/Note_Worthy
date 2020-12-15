b6da38736a139f63972e0a70392afa51d2052f80
tt4623 <sltebr33zy@gmail.com>
2020-12-14 18:19:25 -0600

    added route for not-existent pages (to a 404)

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -9,4 +9,9 @@ module.exports = app => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
+
+  app.get('*', (req, res) => {
+    res.status(404);
+    res.sendFile(path.join(__dirname, "../public/404.html"));
+  });
 };
