835cf882e6724028de15d2c38b0137bef5c7fd8b
tt4623 <sltebr33zy@gmail.com>
2020-12-18 17:41:45 -0600

    got rid of 404

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # __Note_Worthy__
 
 Note worthy is a web application to write, and keep track of notes. Pretty simple.
-
-
+[Link to this app](https://git.heroku.com/guarded-dusk-40541.git)
 
 
 
--- routes/html_routes.js
+++ routes/html_routes.js
@@ -2,16 +2,11 @@ const path = require('path');
 
 module.exports = app => {
   // API GET REQUESTS
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-
-  app.get('*', (req, res) => {
-    res.status(404);
-    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
